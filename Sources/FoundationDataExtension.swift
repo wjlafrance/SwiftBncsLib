214bd65704ffaba6732b99141b208ba7aaf16159
William LaFrance <wjlafrance@gmail.com>
2017-04-23 19:35:54 -0500

    extended Foundation.Data to expose contents as a byte array and hex description

---
+++ Sources/FoundationDataExtension.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+extension Foundation.Data {
+
+    func arrayOfBytes() -> [UInt8] {
+        let count = self.count / MemoryLayout<UInt8>.size
+        var bytesArray = [UInt8](repeating: 0, count: count)
+        copyBytes(to: &bytesArray, count: count * MemoryLayout<UInt8>.stride)
+        return bytesArray
+    }
+
+    public var hexDescription: String {
+        let contents = arrayOfBytes()
+        let lineLength = 16
+        return ((0...(contents.count / lineLength)).map { lineIndex in
+            let hexPrintout = ((0..<lineLength).flatMap { columnIndex in
+                let arrayIndex = lineIndex * 16 + columnIndex
+                if arrayIndex >= contents.count { return "   " }
+                return String(format: "%02X ", contents[arrayIndex])
+            }).joined(separator: "")
+            let charPrintout = ((0..<lineLength).flatMap { columnIndex in
+                let arrayIndex = lineIndex * 16 + columnIndex
+                if arrayIndex >= contents.count { return " " }
+                if isprint(Int32(contents[arrayIndex])) == 0 { return "." }
+                return String(format: "%c", contents[arrayIndex])
+            }).joined(separator: "")
+
+            return String(format: "%04X: ", lineIndex * lineLength) + "\(hexPrintout)   \(charPrintout)"
+        }).joined(separator: "\n")
+    }
+
+}
---
+++ Tests/SwiftBncsLibTests/FoundationDataExtensionTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+@testable import SwiftBncsLib
+
+class FoundationDataExtensionTests: XCTestCase {
+
+    func testArrayOfBytes() {
+        let inputBytes: [UInt8] = [1, 2, 3, 127, 255, 0]
+
+        let testData = Foundation.Data(bytes: inputBytes)
+
+        XCTAssertEqual(testData.arrayOfBytes(), inputBytes)
+    }
+
+    func testHexDescription() {
+        let inputBytes = [UInt8](1...255)
+        let testData = Foundation.Data(bytes: inputBytes)
+
+        let test = testData.hexDescription
+        print(test)
+
+        // Printable characters
+        XCTAssertEqual(test.components(separatedBy: "\n")[2],
+           "0020: 21 22 23 24 25 26 27 28 29 2A 2B 2C 2D 2E 2F 30    !\"#$%&'()*+,-./0")
+
+        // Non-printable characters
+        // Handling non-full lines
+        XCTAssertEqual(test.components(separatedBy: "\n")[15],
+           "00F0: F1 F2 F3 F4 F5 F6 F7 F8 F9 FA FB FC FD FE FF       ............... ")
+    }
+
+}
