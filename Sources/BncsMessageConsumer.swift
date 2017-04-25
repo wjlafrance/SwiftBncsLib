1bddb20d5a3ed16af94b25a13b2b4a0fb1979d75
William LaFrance <wjlafrance@gmail.com>
2017-04-25 17:12:02 -0500

    BncsMessageConsumer supports reading uint8/16/32/64

--- Sources/BncsMessageConsumer.swift
+++ Sources/BncsMessageConsumer.swift
@@ -9,5 +9,20 @@ class BncsMessageConsumer {
         self.message = message
         self.readIndex = 4
     }
-    
+
+    func readUInt8() -> UInt8 {
+        let x = message.data.arrayOfBytes()[readIndex]
+        readIndex += 1
+        return x
+    }
+    func readUInt16() -> UInt16 {
+        return UInt16(readUInt8()) | UInt16(readUInt8()) << 8
+    }
+    func readUInt32() -> UInt32 {
+        return UInt32(readUInt16()) | UInt32(readUInt16()) << 16
+    }
+    func readUInt64() -> UInt64 {
+        return UInt64(readUInt32()) | UInt64(readUInt32()) << 32
+    }
+
 }
--- Tests/SwiftBncsLibTests/BncsMessageConsumerTests.swift
+++ Tests/SwiftBncsLibTests/BncsMessageConsumerTests.swift
@@ -3,16 +3,41 @@ import XCTest
 
 class BncsMessageConsumerTests: XCTestCase {
 
-    func testInitializationStoresMessage() {
-        let testConsumer = BncsMessageConsumer(message: BncsMessage.examplePingMessage)
+    func generateTestConsumer() -> BncsMessageConsumer {
+        return BncsMessageConsumer(message: BncsMessage.exampleRegistryMessage)
+    }
 
-        XCTAssertEqual(testConsumer.message, BncsMessage.examplePingMessage)
+    func testInitializationStoresMessage() {
+        let testConsumer = generateTestConsumer()
+
+        XCTAssertEqual(testConsumer.message, BncsMessage.exampleRegistryMessage)
     }
 
     func testInitializationMovesReadIndex() {
-        let testConsumer = BncsMessageConsumer(message: BncsMessage.examplePingMessage)
+        let testConsumer = generateTestConsumer()
 
         XCTAssertEqual(testConsumer.readIndex, 4, "read index should be past header")
+    }
+
+    func testReadUInt8() {
+        let testU8Consumer = generateTestConsumer()
+        XCTAssertEqual(0xDE, testU8Consumer.readUInt8())
+        XCTAssertEqual(5, testU8Consumer.readIndex)
+    }
+    func testReadUInt16() {
+        let testU16Consumer = generateTestConsumer()
+        XCTAssertEqual(0xADDE, testU16Consumer.readUInt16())
+        XCTAssertEqual(4 + (16 / 8), testU16Consumer.readIndex)
+    }
+    func testReadUInt32() {
+        let testU32Consumer = generateTestConsumer()
+        XCTAssertEqual(0xEFBEADDE, testU32Consumer.readUInt32())
+        XCTAssertEqual(4 + (32 / 8), testU32Consumer.readIndex)
+    }
+    func testReadUInt64() {
+        let testU64Consumer = generateTestConsumer()
+        XCTAssertEqual(0x80000001EFBEADDE, testU64Consumer.readUInt64())
+        XCTAssertEqual(4 + (64 / 8), testU64Consumer.readIndex)
     }
 
 }
