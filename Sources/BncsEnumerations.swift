e536a4ce1756c94d321693c4d0088cbe7b67f7c4
William LaFrance <wjlafrance@gmail.com>
2017-05-04 20:32:20 -0500

    extended enums, corrected endianness

--- Sources/BncsEnumerations.swift
+++ Sources/BncsEnumerations.swift
@@ -8,9 +8,39 @@ enum BncsMessageIdentifier: UInt8 {
 }
 
 enum BncsPlatformIdentifier: UInt32 {
-    case IntelX86 = 0x36385849
+    case IntelX86       = 0x49583836 // IX86
+    case PowerMacintosh = 0x504d4143 // PMAC
+    case IntelMacintosh = 0x584d4143 // XMAC
+}
+
+enum BncsLanguageIdentifier: UInt32 {
+    case Nil = 0
+    case EnglishUnitedStates = 0x656e5553 // enUS
 }
 
 enum BncsProductIdentifier: UInt32 {
-    case StarcraftJapan = 0x5254534a
+
+    case Telnet             = 0x43484154 // CHAT
+    case StarcraftShareware = 0x53534852 // SSHR
+    case StarcraftJapan     = 0x4a535452 // JSTR
+    case Starcraft          = 0x53544152 // STAR
+    case StarcraftExpansion = 0x53455850 // SEXP
+    case DiabloShareware    = 0x44534852 // DSHR
+    case DiabloBeta         = 0x44494142 // DIAB
+    case DiabloStressTest   = 0x44545354 // DTST
+    case Diablo             = 0x4452544c // DRTL
+    case Diablo2StressTest  = 0x44325354 // D2ST
+    case Diablo2            = 0x44324456 // D2DV
+    case Diablo2Expansion   = 0x44325850 // D2XP
+    case Warcraft2          = 0x5732424e // W2BN
+    case Warcraft3Demo      = 0x5733444d // W3DM
+    case Warcraft3          = 0x57415233 // WAR3
+    case Warcraft3Expansion = 0x57335850 // W3XP
+
+    var isValidBncsClient: Bool {
+        let invalidBncsClients: [BncsProductIdentifier] = [.Telnet, .Starcraft, .StarcraftExpansion, .StarcraftShareware, .StarcraftJapan, .DiabloShareware, .DiabloBeta, .DiabloStressTest, .Diablo2StressTest, .Warcraft3Demo]
+
+        return !invalidBncsClients.contains(self)
+    }
+
 }
