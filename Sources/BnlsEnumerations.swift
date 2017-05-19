113829d870c3a49c525de91c266fc1338bc1f9d2
William LaFrance <wjlafrance@gmail.com>
2017-05-18 19:45:41 -0500

    BNLS product identifiers

--- Sources/BnlsEnumerations.swift
+++ Sources/BnlsEnumerations.swift
@@ -5,3 +5,14 @@ public enum BnlsMessageIdentifier: UInt8 {
     case VersionCheckEx2    = 0x1A
     case Unknown            = 0xFF
 }
+
+public enum BnlsProductIdentifier: UInt32 {
+    case Starcraft          = 1
+    case StarcraftExpansion = 2
+    case Warcraft2          = 3
+    case Diablo2            = 4
+    case Diablo2Expansion   = 5
+    case StarcraftJapan     = 6
+    case Warcraft3          = 7
+    case Warcraft3Expansion = 8
+}
